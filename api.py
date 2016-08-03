062a4395dd8e4418e3f540c0e210f6ff36f229dc
kellim <kelli.blalock2@gmail.com>
2016-08-02 23:39:57 -0700

    Add user endpoint

---
+++ api.py
@@ -0,0 +1,30 @@
+import logging
+import endpoints
+from protorpc import remote, messages
+
+from models import User
+from models import StringMessage
+
+
+USER_REQUEST = endpoints.ResourceContainer(user_name=messages.StringField(1),
+                                           email=messages.StringField(2))
+
+@endpoints.api(name='hangman', version='v1')
+class HangmanApi(remote.Service):
+    """Game API"""
+    @endpoints.method(request_message=USER_REQUEST,
+                      response_message=StringMessage,
+                      path='user',
+                      name='create_user',
+                      http_method='POST')
+    def create_user(self,request):
+        """Create a User. Requires a unique username"""
+        if User.query(User.name == request.user_name).get():
+            raise endpoints.ConflictException(
+                'A User with that name already exists!')
+        user = User(name=request.user_name, email=request.email)
+        user.put()
+        return StringMessage(message='User {} created!'.format(
+            request.user_name))
+
+api = endpoints.api_server([HangmanApi])
