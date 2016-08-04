9f17c56c3ca76015df32187b58512b75a9cef793
kellim <kelli.blalock2@gmail.com>
2016-08-03 20:55:56 -0700

    Add new_game endpoint
    Added new_game endpoint with some test data for secret word.

--- api.py
+++ api.py
@@ -2,12 +2,14 @@ import logging
 import endpoints
 from protorpc import remote, messages
 
-from models import User
-from models import StringMessage
+from models import User, Game
+from models import StringMessage, NewGameForm, GameForm
 
 
 USER_REQUEST = endpoints.ResourceContainer(user_name=messages.StringField(1),
                                            email=messages.StringField(2))
+NEW_GAME_REQUEST = endpoints.ResourceContainer(NewGameForm)
+
 
 @endpoints.api(name='hangman', version='v1')
 class HangmanApi(remote.Service):
@@ -17,7 +19,7 @@ class HangmanApi(remote.Service):
                       path='user',
                       name='create_user',
                       http_method='POST')
-    def create_user(self,request):
+    def create_user(self, request):
         """Create a User. Requires a unique username"""
         if User.query(User.name == request.user_name).get():
             raise endpoints.ConflictException(
@@ -26,5 +28,25 @@ class HangmanApi(remote.Service):
         user.put()
         return StringMessage(message='User {} created!'.format(
             request.user_name))
+
+    @endpoints.method(request_message=NEW_GAME_REQUEST,
+                      response_message=GameForm,
+                      path='game',
+                      name='new_game',
+                      http_method='POST')
+    def new_game(self, request):
+        """Creates new game"""
+        user = User.query(User.name == request.user_name).get()
+        if not user:
+            raise endpoints.NotFoundException(
+                'A User with that name does not exist!')
+        try:
+            game = Game.new_game(user.key, request.allowed_misses)
+        except ValueError:
+            raise endpoints.BadRequestException('Allowed misses must be '
+                                                ' between 6 and 10!')
+
+        return game.to_form('Enjoy playing Hangman!')
+
 
 api = endpoints.api_server([HangmanApi])
--- models.py
+++ models.py
@@ -5,7 +5,63 @@ from google.appengine.ext import ndb
 class User(ndb.Model):
     """User profile"""
     name = ndb.StringProperty(required=True)
-    email =ndb.StringProperty()
+    email = ndb.StringProperty()
+
+
+class Game(ndb.Model):
+    """Game Object"""
+    allowed_misses = ndb.IntegerProperty(required=True, default=6)
+    secret_word = ndb.StringProperty(required=True)
+    difficulty = ndb.IntegerProperty(required=True)
+    guessed_word = ndb.StringProperty(required=True)
+    missed_letters = ndb.StringProperty(repeated=True)
+    misses_left = ndb.IntegerProperty(required=True, default=6)
+    game_over = ndb.BooleanProperty(required=True, default=False)
+    user = ndb.KeyProperty(required=True, kind='User')
+
+    @classmethod
+    def new_game(cls, user, allowed_misses):
+        if allowed_misses < 6 or allowed_misses > 10:
+            raise ValueError('Allowed misses must be between 6 and 10')
+        game = Game(user=user,
+                    allowed_misses=allowed_misses,
+                    secret_word="test",
+                    difficulty=1,
+                    guessed_word="----",
+                    misses_left=allowed_misses,
+                    game_over=False)
+        game.put()
+        return game
+
+    def to_form(self, message):
+        """Retuns a GameForm representation of the Game"""
+        form = GameForm()
+        form.urlsafe_key = self.key.urlsafe()
+        form.user_name = self.user.get().name
+        form.misses_left = self.misses_left
+        form.missed_letters = self.missed_letters
+        form.guessed_word = self.guessed_word
+        form.game_over = self.game_over
+        form.message = message
+        return form
+
+
+class GameForm(messages.Message):
+    """GameForm for outbound game state information"""
+    urlsafe_key = messages.StringField(1, required=True)
+    misses_left = messages.IntegerField(2, required=True)
+    missed_letters = messages.StringField(3, repeated=True)
+    guessed_word = messages.StringField(4, required=True)
+    game_over = messages.BooleanField(5, required=True)
+    message = messages.StringField(6, required=True)
+    user_name = messages.StringField(7, required=True)
+
+
+class NewGameForm(messages.Message):
+    """Used to create a new game"""
+    user_name = messages.StringField(1, required=True)
+    allowed_misses = messages.IntegerField(2, default=6)
+
 
 class StringMessage(messages.Message):
     """StringMessage-- outbound (single) string message"""
