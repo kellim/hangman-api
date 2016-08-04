a7ff9278b803421b4d48becf446f06e098225e05
kellim <kelli.blalock2@gmail.com>
2016-08-04 13:15:43 -0700

    Add get_game endpoint

--- api.py
+++ api.py
@@ -4,10 +4,14 @@ from protorpc import remote, messages
 
 from models import User, Game
 from models import StringMessage, NewGameForm, GameForm
+from utils import get_by_urlsafe
 
 
 USER_REQUEST = endpoints.ResourceContainer(user_name=messages.StringField(1),
                                            email=messages.StringField(2))
+GET_GAME_REQUEST = endpoints.ResourceContainer(
+    urlsafe_game_key=messages.StringField(1))
+
 NEW_GAME_REQUEST = endpoints.ResourceContainer(NewGameForm)
 
 
@@ -48,5 +52,20 @@ class HangmanApi(remote.Service):
 
         return game.to_form('Enjoy playing Hangman!')
 
+    @endpoints.method(request_message=GET_GAME_REQUEST,
+                      response_message=GameForm,
+                      path='game/{urlsafe_game_key}',
+                      name='get_game',
+                      http_method='GET')
+    def get_game(self,request):
+        """Return the current game state."""
+        game = get_by_urlsafe(request.urlsafe_game_key, Game)
+        if game:
+            if not game.game_over:
+                return game.to_form('It\'s time to take a turn!')
+            else:
+                return game.to_form('The game is over!')
+        else:
+            raise endpoints.NotFoundException('No game was found!')
 
 api = endpoints.api_server([HangmanApi])
